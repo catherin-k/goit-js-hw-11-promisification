121274a7755e0f1c5cfc489ecbd76bc7b512b74a
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2020-12-24 16:16:37 +0200

    add final touches

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import css from "./css/styles.css";
-// import task1 from "./js/task-1.js";
-// import task2 from "./js/task-2.js";
+import task1 from "./js/task-1.js";
+import task2 from "./js/task-2.js";
 import task3 from "./js/task-3.js";
